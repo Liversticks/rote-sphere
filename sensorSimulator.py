15fef6c49896f48d7be0ae3713232113c6259a50
Thomas-Kingsman <90071802+Thomas-Kingsman@users.noreply.github.com>
2021-09-04 01:10:54 -0700

    Update sensorSimulator.py
    slight tweaks to included fields and ints generated

--- sensorSimulator.py
+++ sensorSimulator.py
@@ -3,9 +3,10 @@ import random
 import time
 
 class sensor():  
-  def __init__(self, uid, type_, address, unit, street, city, country, zippostcode):
+  def __init__(self, uid, type_, resource, address, unit, street, city, country, zippostcode):
     self.uid = uid
     self.type = type_
+    self.resource = resource
     self.address = address
     self.unit = unit
     self.street = street
@@ -14,10 +15,10 @@ class sensor():
     self.zippostcode = zippostcode
     
     
-tester1 = sensor(uuid.uuid4(), 'Appliance', 12345, 1, 'Notareal St', 'Notareal City', 'Atlantis', 'H0H0H0')
+tester1 = sensor(uuid.uuid4(), 'Appliance', 'Resource', 12345, 1, 'Notareal St', 'Notareal City', 'Atlantis', 'H0H0H0')
 random.seed()
 
 while True: 
-    gasused = random.randint(0, 10)
+    powerused = random.randint(0, 10)
     waterused = random.randint(0, 10)
     time.sleep(3600)
