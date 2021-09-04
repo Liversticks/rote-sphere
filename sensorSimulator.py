ab200841b372119c9d59ee09aea081c744ec79dd
Thomas-Kingsman <90071802+Thomas-Kingsman@users.noreply.github.com>
2021-09-04 00:04:54 -0700

    Update sensorSimulator.py

--- sensorSimulator.py
+++ sensorSimulator.py
@@ -3,11 +3,18 @@ import random
 import time
 
 class sensor():  
-  def __init__(self, uid, type_):
+  def __init__(self, uid, type_, address, unit, street, city, country, zippostcode):
     self.uid = uid
     self.type = type_
+    self.address = address
+    self.unit = unit
+    self.street = street
+    self.city = city
+    self.country = country 
+    self.zippostcode = zippostcode
     
-tester1 = sensor(uuid.uuid4(), 'Appliance')
+    
+tester1 = sensor(uuid.uuid4(), 'Appliance', 12345, 1, 'Notareal St', 'Notareal City', 'Atlantis', 'H0H0H0')
 random.seed()
 
 while True: 
