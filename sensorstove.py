85fd59532801b292cb76662f02cc157134b68fe8
Thomas-Kingsman <90071802+Thomas-Kingsman@users.noreply.github.com>
2021-09-04 10:59:14 -0700

    Update sensorstove.py

--- sensorstove.py
+++ sensorstove.py
@@ -24,7 +24,7 @@ class sensorData():
     self.power = power
     
     
-tester1 = sensor(uuid.uuid4(), 'Stove', 'Power', 12345, 1, 'Notareal St', 'Notareal City', 'Atlantis', 'A1A1A1')
+tester1 = sensor(str(uuid.uuid4()), 'Stove', 'Power', 12345, 1, 'Notareal St', 'Notareal City', 'Atlantis', 'A1A1A1')
 random.seed()
 
 while True: 
@@ -35,6 +35,6 @@ while True:
   else: 
     powerused = 0
   waterused = 0
-  package = sensorData(tester1.uid, datetime.now(timezone('America/Vancouver')), waterused, powerused)
+  package = sensorData(tester1.uid, str(datetime.now(timezone('America/Vancouver'))), waterused, powerused)
   jsonStr = json.dumps(package.__dict__)
   time.sleep(28800) #8 hours, or 1/3 day
