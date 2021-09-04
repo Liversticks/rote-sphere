e6c6478b877d9c87899be952189c03a2e445aa71
Oliver Xie <oliverxie19@gmail.com>
2021-09-03 23:39:50 -0700

    django - initial models and migration

--- rote_sphere_web/game_data/models.py
+++ rote_sphere_web/game_data/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Sensor(models.Model):
+    linked_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    id = models.UUIDField(primary_key=True, editable=False)
+
+class DayUsage(models.Model):
+    linked_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    day = models.DateTimeField('tracked day')
+    water = models.DecimalField('water usage', default=0, max_digits=14, decimal_places=10)
+    power = models.DecimalField('power + gas usage', default=0, max_digits=14, decimal_places=10)
+    # TODO: track a monetary cost per day?
